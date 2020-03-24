0814929e8db17ebbb9249cced8a7130db7e1c6ae
Claire Kodia <claire.kodia@gmail.com>
2020-03-24 10:57:56 +0100

    Solved exercices fizzbuzz with Emily D.

--- fizzBuzz.js
+++ fizzBuzz.js
@@ -1,5 +1,21 @@
-const fizzBuzz = () => {
-  // Start here
+const fizzBuzz = (number) => {
+    const multipl3 = number % 3 === 0;
+    const multipl5 = number % 5 === 0;
+
+    if (multipl3 && multipl5) {
+        return "fizzbuzz"
+    } else if (multipl3) {
+        return "fizz"
+    } else if (multipl5) {
+        return "buzz"
+    } else {
+        return number
+    }
 };
 
+
+console.log(fizzBuzz(15));
+console.log(fizzBuzz(5));
+console.log(fizzBuzz(2));
+console.log(fizzBuzz(3));
 module.exports = fizzBuzz;
